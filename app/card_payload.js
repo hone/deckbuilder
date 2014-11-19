faa3adf9343a4e25d5ff86ad491fe7822d85f36c
Brandon Hays <brandon.hays@gmail.com>
2014-11-19 15:59:12 -0800

    move payload to nice es6 cool thing

--- app/card_payload.js
+++ app/card_payload.js
@@ -1,1 +1,182 @@
+var cardPayload = [
+  {
+    Title: "Aragorn",
+    IsUnique: true,
+    CardType: "Hero",
+    CardSubType: "None",
+    Sphere: "Leadership",
+    Front: {
+      Subtitle: null,
+      ImagePath: "http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.png",
+      Stats: {
+        ThreatCost: "12",
+        Willpower: "2",
+        Attack: "3",
+        Defense: "2",
+        HitPoints: "5"
+      },
+      Traits: [
+        "Dúnedain.",
+        "Noble.",
+        "Ranger."
+      ],
+      Keywords: [
+        "Sentinel."
+      ],
+      Text: [
+        "Response: After Aragorn commits to a quest, spend 1 resource from his resource pool to ready him."
+      ],
+      FlavorText: "`I am Aragorn son of Arathorn; and if by life or death I can save you, I will.` -The Fellowship of the Ring"
+    },
+    Back: null,
+    CardSet: "Core Set",
+    EncounterInfo: null,
+    Number: 1,
+    Quantity: 1,
+    Artist: "John Stanko",
+    HasErrata: false
+  },
+  {
+    Title: "Théodred",
+    IsUnique: true,
+    CardType: "Hero",
+    CardSubType: "None",
+    Sphere: "Leadership",
+    Front: {
+      Subtitle: null,
+      ImagePath: "http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Théodred.png",
+      Stats: {
+        ThreatCost: "8",
+        Willpower: "1",
+        Attack: "2",
+        Defense: "1",
+        HitPoints: "4"
+      },
+      Traits: [
+        "Noble.",
+        "Rohan.",
+        "Warrior."
+      ],
+      Keywords: [ ],
+      Text: [
+        "Response: After Théodred commits to a quest, choose a hero committed to that quest. Add 1 resource to that hero's resource pool."
+      ],
+      FlavorText: "`Not all is dark. Take courage, Lord of the Mark...` -Gandalf, The Two Towers"
+    },
+    Back: null,
+    CardSet: "Core Set",
+    EncounterInfo: null,
+    Number: 2,
+    Quantity: 1,
+    Artist: "Jeff Himmelman",
+    HasErrata: false
+  },
+  {
+    Title: "Glóin",
+    IsUnique: true,
+    CardType: "Hero",
+    CardSubType: "None",
+    Sphere: "Leadership",
+    Front: {
+      Subtitle: null,
+      ImagePath: "http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Glóin.png",
+      Stats: {
+        ThreatCost: "9",
+        Willpower: "2",
+        Attack: "2",
+        Defense: "1",
+        HitPoints: "4"
+      },
+      Traits: [
+        "Dwarf.",
+        "Noble."
+      ],
+      Keywords: [ ],
+      Text: [
+        "Response: After Glóin suffers damage, add 1 resource to his resource pool for each point of damage he just suffered."
+      ],
+      FlavorText: null
+    },
+    Back: null,
+    CardSet: "Core Set",
+    EncounterInfo: null,
+    Number: 3,
+    Quantity: 1,
+    Artist: "Tom Garden",
+    HasErrata: false
+  },
+  {
+    Title: "Gimli",
+    IsUnique: true,
+    CardType: "Hero",
+    CardSubType: "None",
+    Sphere: "Tactics",
+    Front: {
+      Subtitle: null,
+      ImagePath: "http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Gimli.png",
+      Stats: {
+        ThreatCost: "11",
+        Willpower: "2",
+        Attack: "2",
+        Defense: "2",
+        HitPoints: "5"
+      },
+      Traits: [
+        "Dwarf.",
+        "Noble.",
+        "Warrior."
+      ],
+      Keywords: [ ],
+      Text: [
+        "Gimli gets +1 Attack for each damage token on him."
+      ],
+      FlavorText: "`Men need many words before deeds. My axe is restless in my hands` -The Two Towers"
+    },
+    Back: null,
+    CardSet: "Core Set",
+    EncounterInfo: null,
+    Number: 4,
+    Quantity: 1,
+    Artist: "Tony Foti",
+    HasErrata: false
+  },
+  {
+    Title: "Legolas",
+    IsUnique: true,
+    CardType: "Hero",
+    CardSubType: "None",
+    Sphere: "Tactics",
+    Front: {
+      Subtitle: null,
+      ImagePath: "http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Legolas.png",
+      Stats: {
+        ThreatCost: "9",
+        Willpower: "1",
+        Attack: "3",
+        Defense: "1",
+        HitPoints: "4"
+      },
+      Traits: [
+        "Noble.",
+        "Silvan.",
+        "Warrior."
+      ],
+      Keywords: [
+        "Ranged."
+      ],
+      Text: [
+        "Response: After Legolas participates in an attack that destroys an enemy, place 2 progress tokens on the current quest."
+      ],
+      FlavorText: null
+    },
+    Back: null,
+    CardSet: "Core Set",
+    EncounterInfo: null,
+    Number: 5,
+    Quantity: 1,
+    Artist: "Magali Villeneuve",
+    HasErrata: false
+  }
+]
+
 export default cardPayload;
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,17 +1,17 @@
 import Ember from 'ember';
+import cardPayload from '../card_payload';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-	var cardPayload = [{"id":"aragorn-core-set","Title":"Aragorn","IsUnique":true,"CardType":"Hero","CardSubType":"None","Sphere":"Leadership","Front":{"Subtitle":null,"ImagePath":"http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Aragorn.png","Stats":{"ThreatCost":"12","Willpower":"2","Attack":"3","Defense":"2","HitPoints":"5"},"Traits":["Dúnedain.","Noble.","Ranger."],"Keywords":["Sentinel."],"Text":["Response: After Aragorn commits to a quest, spend 1 resource from his resource pool to ready him."],"FlavorText":"`I am Aragorn son of Arathorn; and if by life or death I can save you, I will.` -The Fellowship of the Ring"},"Back":null,"CardSet":"Core Set","EncounterInfo":null,"Number":1,"Quantity":1,"Artist":"John Stanko","HasErrata":false},{"Title":"Théodred","IsUnique":true,"CardType":"Hero","CardSubType":"None","Sphere":"Leadership","Front":{"Subtitle":null,"ImagePath":"http://hallofbeorn.comhttps://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/Core-Set/Théodred.png","Stats":{"ThreatCost":"8","Willpower":"1","Attack":"2","Defense":"1","HitPoints":"4"},"Traits":["Noble.","Rohan.","Warrior."],"Keywords":[],"Text":["Response: After Théodred commits to a quest, choose a hero committed to that quest. Add 1 resource to that hero\u0027s resource pool."],"FlavorText":"`Not all is dark. Take courage, Lord of the Mark...`\r\n\r\n-Gandalf, The Two Towers"},"Back":null,"CardSet":"Core Set","EncounterInfo":null,"Number":2,"Quantity":1,"Artist":"Jeff Himmelman","HasErrata":false}];
+	  this.cardPayload = cardPayload.map(function(item) {
+	    item.id = (item.Title + "-" + item.CardSet).dasherize();
+	    item.Front.ImagePath = item.Front.ImagePath.replace(/^http:\/\/hallofbeorn.com/, '');
 
-	this.cardPayload = cardPayload.map(function(item) {
-	  //item.id = (item.Title + "-" + item.CardSet).dasherize();
-	  item.Front.ImagePath = item.Front.ImagePath.replace(/^http:\/\/hallofbeorn.com/, '');
-
-	  return Ember.Object.create(item);
-	});
+	    return Ember.Object.create(item);
+    });
   },
+
   model: function() {
-	return this.cardPayload;
+    return this.cardPayload;
   }
 });
